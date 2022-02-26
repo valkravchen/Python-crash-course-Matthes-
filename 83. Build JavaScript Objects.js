c5e95dca21cbc0a59724240847295f883af5044b
Пользователь <kravchenkoval@mail.ru>
2022-02-26 14:27:20 +0300

    add new files

---
+++ 83. Build JavaScript Objects.js
@@ -0,0 +1,9 @@
+const myDog = {
+    // Only change code below this line
+  name: 'Tuzik',
+  legs: 4,
+  tails: 1,
+  friends:['People', 'Elephants']
+  
+    // Only change code above this line
+  };
