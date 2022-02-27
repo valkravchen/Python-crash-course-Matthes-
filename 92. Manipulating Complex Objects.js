7e7429538147f69a57b8f349a12f680d350858bd
Пользователь <kravchenkoval@mail.ru>
2022-02-27 09:31:56 +0300

    add new files

---
+++ 92. Manipulating Complex Objects.js
@@ -0,0 +1,20 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+      {"artist": "James Last",
+      "title": "Beachparty",
+      "release_year": 1970,
+      "formats": [
+        "LP", "8T"]
+        
+      }
+  ];
