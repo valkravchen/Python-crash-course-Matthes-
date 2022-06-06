d2c1fbdf218eea407199aeda31c454987d0f7880
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-06-06 19:57:03 +0300

    Delete 83. Build JavaScript Objects.js

--- 83. Build JavaScript Objects.js
+++
@@ -1,9 +0,0 @@
-const myDog = {
-    // Only change code below this line
-  name: 'Tuzik',
-  legs: 4,
-  tails: 1,
-  friends:['People', 'Elephants']
-  
-    // Only change code above this line
-  };
