9014034b6fb5774ea825555f603e7168add2ddea
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-07-03 18:56:02 +0300

    Update greeter.py

--- greeter.py
+++ greeter.py
@@ -1,4 +1,17 @@
-def greet_user(username):
-    print(f"Hello, {username.title()}!")
+def get_formatted_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
 
-greet_user('viv')
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+
+    l_name  = input("Last name: ")
+    if l_name == 'q':
+        break
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
